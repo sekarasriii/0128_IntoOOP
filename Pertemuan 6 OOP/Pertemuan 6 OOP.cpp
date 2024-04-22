c9269efc432e08761187dd8842378d5a09124d22
sekarasriii <sekareeesh@gmail.com>
2024-04-22 15:06:51 +0700

    masukkan fungsi void output untuk class Mahasiswa

--- Pertemuan 6 OOP/Pertemuan 6 OOP.cpp
+++ Pertemuan 6 OOP/Pertemuan 6 OOP.cpp
@@ -7,4 +7,9 @@ public: // akses modifier
 	int umur;
 	string jurusan;
 
+	void output() {
+		cout << "Nama: " << nama << endl;
+		cout << "Umur: " << umur << endl;
+		cout << "Jurusan: " << jurusan << endl;
+	}
 };

94ccb1a0767fe478a450af420505be7c83b5ffef
sekarasriii <sekareeesh@gmail.com>
2024-04-22 15:18:55 +0700

    masukkan fungsi void display

--- Pertemuan OOP_2/Pertemuan OOP_2.cpp
+++ Pertemuan OOP_2/Pertemuan OOP_2.cpp
@@ -17,4 +17,10 @@ public:
 	float hitungLuas() {
 		return panjang * lebar;
 	}
+
+	void display() {
+		cout << "Panjangnya = " << panjang << endl;
+		cout << "Lebarnya = " << lebar << endl;
+		cout << "Luasnya = " << hitungLuas() << endl;
+	}
 };
